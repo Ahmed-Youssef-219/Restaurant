a0f72271c20e30c67ca8ef60a7ea4bcb6da53751
Ahmed-Youssef-219 <ahmed7777youssef@example.com>
2023-03-06 22:17:36 +0200

    add a final detail

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -1,44 +1,49 @@
 import React from 'react';
-import {SiGmail} from 'react-icons/si';
-import {BsFacebook} from 'react-icons/bs';
-import {AiFillInstagram, AiOutlineTwitter} from 'react-icons/ai'
+import { SiGmail } from 'react-icons/si';
+import { BsFacebook } from 'react-icons/bs';
+import { AiFillInstagram, AiOutlineTwitter } from 'react-icons/ai';
+import { Link } from "react-router-dom";
 
 const Footer = () => {
+
+  const handleLink = () => {
+    window.scrollTo(0, 0);
+  }
   return (
     <div className='text-slate-900 dark:bg-slate-800 dark:text-white w-screen py-3 px-16'>
       <div className='flex flex-col sm:flex-row items-center justify-center sm:justify-between '>
         <h1 className='text-5xl font-bold p-3'>Gusto</h1>
-          
-          <div className='p-3'>
-            <h3 className='text-xl font-bold text-center py-3'>Useful Links</h3>
-            <ul className='text-lg'>
-              <li className='hover:underline hover:font-bold cursor-pointer'>Home</li>
-              <li className='hover:underline hover:font-bold cursor-pointer'>Our Dishes</li>
-              <li className='hover:underline hover:font-bold cursor-pointer'>About</li>
-              <li className='hover:underline hover:font-bold cursor-pointer'>Contact</li>
-            </ul>
-          </div>
-          <div className='p-3'>
-            <h3 className='text-xl font-bold py-3'>Contact via</h3>
-            <ul className='text-lg w-full'>
-              <li className='flex items-center justify-between hover:underline hover:font-bold cursor-pointer'>
-                <BsFacebook className='text-lg' />
-                <p>facebook</p>
-              </li>
-              <li className='flex items-center justify-between hover:underline hover:font-bold cursor-pointer'>
-                <AiFillInstagram className='text-lg'/>
-                <p>Instagram</p>
-              </li>
-              <li className='flex items-center justify-between hover:underline hover:font-bold cursor-pointer'>
-                <AiOutlineTwitter className='text-lg'/>
-                <p>Twitter</p>
-              </li>
-              <li className='flex items-center justify-between hover:underline hover:font-bold cursor-pointer'>
-                <SiGmail className='text-lg'/>
-                <p>G-mail</p>
-              </li>
-            </ul>
-          </div>
+
+        <div className='p-3'>
+          <h3 className='text-xl font-bold text-center py-3'>Useful Links</h3>
+          <ul className='text-lg'>
+            <li className='hover:underline hover:font-bold cursor-pointer'><Link onClick={handleLink} to='/'>Home</Link></li>
+            <li className='hover:underline hover:font-bold cursor-pointer'><Link onClick={handleLink} to='/bestdishes'>Our Dishes</Link></li>
+            <li className='hover:underline hover:font-bold cursor-pointer'><Link onClick={handleLink} to='/about'>About</Link></li>
+            <li className='hover:underline hover:font-bold cursor-pointer'><Link onClick={handleLink} to='/contact'>Contact</Link></li>
+          </ul>
+        </div>
+        <div className='p-3'>
+          <h3 className='text-xl font-bold py-3'>Contact via</h3>
+          <ul className='text-lg w-full'>
+            <li className='flex items-center justify-between hover:underline hover:font-bold cursor-pointer'>
+              <BsFacebook className='text-lg' />
+              <p>facebook</p>
+            </li>
+            <li className='flex items-center justify-between hover:underline hover:font-bold cursor-pointer'>
+              <AiFillInstagram className='text-lg' />
+              <p>Instagram</p>
+            </li>
+            <li className='flex items-center justify-between hover:underline hover:font-bold cursor-pointer'>
+              <AiOutlineTwitter className='text-lg' />
+              <p>Twitter</p>
+            </li>
+            <li className='flex items-center justify-between hover:underline hover:font-bold cursor-pointer'>
+              <SiGmail className='text-lg' />
+              <p>G-mail</p>
+            </li>
+          </ul>
+        </div>
       </div>
       <div>
         <div className='pt-6 pb-2 border-t-4 border-gray-400 text-center'>All Copyrights Reservied || <span className='text-slate-900 font-bold dark:text-white'>AY</span> </div>
--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,11 +1,11 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import { FiMenu } from 'react-icons/fi';
-import {AiOutlineClose} from 'react-icons/ai';
+import { AiOutlineClose } from 'react-icons/ai';
 import { BsFillMoonStarsFill, BsFillSunFill } from 'react-icons/bs';
 import { Link } from "react-router-dom";
 
-const Navbar = ({isDark,setIsDark}) => {
-    
+const Navbar = ({ isDark, setIsDark }) => {
+
     const [isActive, setIsActive] = useState(true)
     const handleMood = () => {
         setIsDark((prevState) => {
@@ -20,17 +20,21 @@ const Navbar = ({isDark,setIsDark}) => {
     }
     window.onscroll = () => {
         setIsActive(true);
-      }
-    
+    }
+
+    const handleLink = () => {
+        window.scrollTo(0, 0);
+    }
+
     return (
         <div className="backdrop-blur-lg z-[100] fixed top-0 right-0 left-0 dark:bg-slate-900/90 bg-white/50 flex items-center justify-between border-black border-b-2 w-screen">
-            <h1 className="font-bold dark:text-slate-200 text-slate-800 py-3 px-4 text-5xl">Gusto</h1>
+            <h1><Link onClick={handleLink} className='font-bold dark:text-slate-200 text-slate-800 py-3 px-4 text-5xl' to='/'>Gusto</Link></h1>
             <ul className={`text-slate-800 dark:text-slate-200 md:items-center md:justify-between w-screen md:w-max flex-col md:flex-row absolute top-[72px] z-50 right-0 left-0 md:static items-end dark:bg-slate-900 bg-white font-bold dark:md:bg-transparent md:bg-transparent dark:backdrop-blur-lg dark:md:backdrop-blur-none  ${isActive ? 'clip-path-hidden' : 'clip-path-visible'} md:clip-path-none list-none flex text-slate-200  text-lg`}>
-                <li className="mx-2 w-full md:w-fit cursor-pointer px-3 py-1 hover:text-white md:hover:rounded hover:scale-95 dark:hover:bg-white dark:hover:text-slate-800 hover:bg-slate-900"><Link className='p-3' to="/">Home</Link></li>
-                <li className="mx-2 w-full md:w-fit cursor-pointer px-3 py-1 hover:text-white md:hover:rounded hover:scale-95 dark:hover:bg-white dark:hover:text-slate-800 hover:bg-slate-900"><Link className='p-3' to="/about">About</Link></li>
-                <li className="break-keep mx-2 w-full md:w-fit cursor-pointer px-3 py-1 hover:text-white md:hover:rounded hover:scale-95 dark:hover:bg-white dark:hover:text-slate-800 hover:bg-slate-900"><Link className='p-3' to="/bestdishes">Best_Dishes</Link></li>
-                <li className="mx-2 w-full md:w-fit cursor-pointer px-3 py-1 hover:text-white md:hover:rounded hover:scale-95 dark:hover:bg-white dark:hover:text-slate-800 hover:bg-slate-900"><Link className='p-3' to="/contact">Contact</Link></li>
-  
+                <li className="mx-2 w-full md:w-fit cursor-pointer px-3 py-1 hover:text-white md:hover:rounded hover:scale-95 dark:hover:bg-white dark:hover:text-slate-800 hover:bg-slate-900"><Link onClick={handleLink} className='p-3' to="/">Home</Link></li>
+                <li className="mx-2 w-full md:w-fit cursor-pointer px-3 py-1 hover:text-white md:hover:rounded hover:scale-95 dark:hover:bg-white dark:hover:text-slate-800 hover:bg-slate-900"><Link onClick={handleLink} className='p-3' to="/about">About</Link></li>
+                <li className="break-keep mx-2 w-full md:w-fit cursor-pointer px-3 py-1 hover:text-white md:hover:rounded hover:scale-95 dark:hover:bg-white dark:hover:text-slate-800 hover:bg-slate-900"><Link onClick={handleLink} className='p-3' to="/bestdishes">Best_Dishes</Link></li>
+                <li className="mx-2 w-full md:w-fit cursor-pointer px-3 py-1 hover:text-white md:hover:rounded hover:scale-95 dark:hover:bg-white dark:hover:text-slate-800 hover:bg-slate-900"><Link onClick={handleLink} className='p-3' to="/contact">Contact</Link></li>
+
             </ul>
             {isDark ? (
                 <div className="md:mx-6 dark:bg-white p-2 rounded-full">
@@ -48,10 +52,10 @@ const Navbar = ({isDark,setIsDark}) => {
                     <AiOutlineClose className="mx-5 md:hidden dark:text-slate-200 text-slate-800 text-xl cursor-pointer" onClick={handleMenu} />
                 )
             }
-            
+
         </div>
     );
-  }
-  
+}
+
 
 export default Navbar
